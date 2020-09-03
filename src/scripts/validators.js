726a459a0708f8b17706de46f619a1109355ffe3
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-09-03 14:41:41 -0400

    module: adds custom auth data validator functions
    to validate login and signup data

---
+++ src/scripts/validators.js
@@ -0,0 +1,54 @@
+// export {errors as default};
+
+
+// declare empty field; to later disallow white spaces in entries
+function isEmpty(string) {
+    if (string.trim() === '') return true;
+    else return false;
+}
+
+// Compare and conform email entry to regular format
+function isEmail(email) {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(regEx)) return true;
+    else return false;
+}
+
+function validateSignupData(data) {
+    let errors = {};
+    if (isEmpty(data.email)) {
+        errors.email = 'Please enter a valid email address'
+    } else if (!isEmail(data.email)) {
+        errors.email = 'Please enter a valid email address'
+    }
+
+    if (isEmpty(data.password)) errors.password = 'Please enter a password';
+    if (isEmpty(data.name)) errors.name = 'Please enter a name for your profile';
+    if (data.password !== data.confirmPassword) errors.confirmPassword = 'Passwords do not match, please try again';
+
+    return {
+        errors,
+        valid: Object.keys(errors).length === 0 ? true : false
+    }
+}
+
+
+function validateLoginData(data) {
+    let errors = {};
+
+    if (isEmpty(data.email)) {
+        errors.email = 'Please enter a valid email address'
+    } else if (!isEmail(data.email)) {
+        errors.email = 'Please enter a valid email address'
+    }
+
+    if (isEmpty(data.password)) errors.password = 'Please enter a password';
+
+    return {
+        errors,
+        valid: Object.keys(errors).length === 0 ? true : false
+    }
+}
+
+
+export { validateLoginData, validateSignupData };
