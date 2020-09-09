5ce9f89510fcb7d4e30b149a3fa66d092ea58d02
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-09-09 07:54:34 -0400

    feat: adds media query for collection page modal @ max width 600px

--- script.js
+++ script.js
@@ -559,6 +559,7 @@ function timeTravel(){
 
 function goToCollections(){
     current_page = "collections"
+    emptyCollection.classList.add("hide");
     emptyCollection.innerHTML = "";
     collectionPage.classList.remove("hide");
 
@@ -578,6 +579,7 @@ function goToCollections(){
         console.log(activeCollection)
         if (activeCollection)
         axiosRetrieveTweets(activeCollection.innerText.trim())
+        console.log("");
     }
 }   
 
@@ -1188,6 +1190,7 @@ function retrieveCollectionTweets(){
     console.log("heyyyyy")
 
     emptyCollection.innerHTML = "";
+    emptyCollection.classList.add("hide");
     let collectionName = event.currentTarget.innerText.trim();
 
     axiosRetrieveTweets(collectionName)
@@ -1229,8 +1232,10 @@ function axiosRetrieveTweets(collection){
 
         
             if (tweetResultsDiv.innerHTML === "") {
+                emptyCollection.classList.remove("hide");
                 emptyCollection.innerHTML = `<span class="color-blue">${collection}</span> currently has no saved tweets`;
-                emptyCollection.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+                // emptyCollection.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+                emptyCollection.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
             }
 
             interactWithSearchResults();
@@ -1726,3 +1731,63 @@ function atlify(text) {
 //         console.log(mention.innerHTML)
 //     })
 // })
+
+
+
+
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+        } else {
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            // if (!(mediaModal.classList.contains("hide"))) {
+            //     history.back();
+            //     state.screenFade.class = screenFade.className;
+            //     state.media.class = mediaModal.className;
+            // }
+            /* up swipe */ 
+        } else { 
+            // if (!(mediaModal.classList.contains("hide"))) {
+            //     history.back();
+            //     state.screenFade.class = screenFade.className;
+            //     state.media.class = mediaModal.className;
+            // }
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
