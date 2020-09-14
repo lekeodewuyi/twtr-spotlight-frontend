05f6da8c02fdf49c30f874a2825ae756a3718ef5
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-09-14 13:27:35 -0400

    feat: adds function to pause palying video when not in the viewport

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1565,6 +1565,17 @@ function interactWithSearchResults(){
         }, false)
     })
 
+    function isInViewport(element) {
+
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
     let allVideos = document.querySelectorAll("video");
     allVideos.forEach((video) => {
         video.addEventListener("play", function(event){
@@ -1575,7 +1586,14 @@ function interactWithSearchResults(){
                 }
             })
         })
+
+        centerContainer.addEventListener("scroll", function(){
+            if (!isInViewport(video)) {
+                video.pause();
+            }
+        }, false)
     })
+
 
     
     // save tweet to colection
