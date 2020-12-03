e844dc1f2c0538e92428c30f91500f05c45c1d2b
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-12-03 13:53:22 -0500

    fix: sanitizes dynamic dom content

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1218,12 +1218,12 @@ function appendTweets(results){
         let tweetText = document.createElement("p");
         tweetText.classList.add("tweet-text");
         if (typeof results[i].retweeted_status === "object") {
-            tweetText.innerText = results[i].retweeted_status.full_text;
-            tweetText.innerHTML = DOMPurify.sanitize(urlify(tweetText.innerText))
+            tweetText.innerHTML = results[i].retweeted_status.full_text;
+            tweetText.innerHTML = DOMPurify.sanitize(urlify(tweetText.innerHTML))
             tweetText.innerHTML = DOMPurify.sanitize(atlify(tweetText.innerText))
         } else {
-            tweetText.innerText = results[i].full_text;
-            tweetText.innerHTML = DOMPurify.sanitize(urlify(tweetText.innerText))
+            tweetText.innerHTML = results[i].full_text;
+            tweetText.innerHTML = DOMPurify.sanitize(urlify(tweetText.innerHTML))
             tweetText.innerHTML = DOMPurify.sanitize(atlify(tweetText.innerText))
         }
 
@@ -1306,7 +1306,7 @@ function appendTweets(results){
 
         let tweetLikesIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         tweetLikesIcon.classList.add("tweet-icon");
-        tweetLikesIcon.innerHTML = DOMPurify.sanitize(`<path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>`);
+        tweetLikesIcon.innerHTML = `<path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>`;
         tweetLikesIcon.setAttribute("viewBox", "0 0 24 24");
         let tweetLikes = document.createElement("p");
         tweetLikes.innerText = `${results[i].retweet_count} `;
