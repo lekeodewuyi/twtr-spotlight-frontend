134646e4de9b9eacee7501c01357d2324778a943
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-12-03 14:00:04 -0500

    fix: sanitizes dynamic dom content

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1219,11 +1219,11 @@ function appendTweets(results){
         tweetText.classList.add("tweet-text");
         if (typeof results[i].retweeted_status === "object") {
             tweetText.innerHTML = results[i].retweeted_status.full_text;
-            tweetText.innerHTML = DOMPurify.sanitize(urlify(tweetText.innerHTML))
+            tweetText.innerHTML = urlify(tweetText.innerHTML)
             tweetText.innerHTML = DOMPurify.sanitize(atlify(tweetText.innerText))
         } else {
             tweetText.innerHTML = results[i].full_text;
-            tweetText.innerHTML = DOMPurify.sanitize(urlify(tweetText.innerHTML))
+            tweetText.innerHTML = urlify(tweetText.innerHTML)
             tweetText.innerHTML = DOMPurify.sanitize(atlify(tweetText.innerText))
         }
 
@@ -1309,7 +1309,7 @@ function appendTweets(results){
         tweetLikesIcon.innerHTML = `<path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>`;
         tweetLikesIcon.setAttribute("viewBox", "0 0 24 24");
         let tweetLikes = document.createElement("p");
-        tweetLikes.innerText = `${results[i].retweet_count} `;
+        tweetLikes.innerText = `${results[i].favorite_count} `;
         tweetLikes.innerText = appendCommas(tweetLikes.innerText);
         tweetMetrics.append(tweetRetweetIcon, tweetRetweets, tweetLikesIcon, tweetLikes);
 
